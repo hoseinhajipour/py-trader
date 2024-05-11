ab621c26213bff4c2ee7d69a035a16759e5d80e5
hajipour <hoseinhajepor@gmail.com>
2024-05-11 23:35:48 +0330

    update js,ui,...

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import MetaTrader5 as mt5
+import time
+import json
 
 app = Flask(__name__)
 
-# Connect to MetaTrader 5 terminal
-mt5.initialize()
 
-def get_account_balance():
+
+# Create a dictionary to store MetaTrader accounts
+meta_trader_accounts = {}
+
+def get_account_balance(account_id):
+    if not  mt5.initialize(path= meta_trader_accounts[account_id]['path'], login=int(meta_trader_accounts[account_id]['login']) , server=meta_trader_accounts[account_id]['server'], password=meta_trader_accounts[account_id]['password']):
+        print("initialize() failed, error code =",mt5.last_error())
+    print(meta_trader_accounts[account_id])
+    #mt5.login(meta_trader_accounts[account_id]['login'], meta_trader_accounts[account_id]['password'],meta_trader_accounts[account_id]['server'])
     account_info = mt5.account_info()
-    balance = account_info.balance
+    balance = 0
+    if account_info :
+        print("account_info x : ",account_info)
+        balance = account_info.balance
+        
+    mt5.shutdown()
     return balance
 
-def place_order(symbol, volume, order_type, price):
+# Load MetaTrader accounts from local storage
+try:
+    with open('mt_accounts.json', 'r') as file:
+        meta_trader_accounts = json.load(file)
+        print(meta_trader_accounts)
+        # Load balances for each account
+        for account_id in meta_trader_accounts:
+            balance = get_account_balance(account_id)
+            meta_trader_accounts[account_id]['balance'] = balance
+except FileNotFoundError:
+    pass
+
+def save_meta_trader_accounts():
+    with open('mt_accounts.json', 'w') as file:
+        json.dump(meta_trader_accounts, file)
+
+
+
+def place_order(account_id, symbol, volume, order_type, price):
+    print("strat place_order \n")
+    #mt5.initialize( path=meta_trader_accounts[account_id]['path'],login=int(meta_trader_accounts[account_id]['login']), server=meta_trader_accounts[account_id]['server'],password=meta_trader_accounts[account_id]['password'])
+    #mt5.login(meta_trader_accounts[account_id]['login'], meta_trader_accounts[account_id]['password'],meta_trader_accounts[account_id]['server'])
     request_data = {
         "action": mt5.TRADE_ACTION_DEAL,
         "symbol": symbol,
@@ -25,9 +59,12 @@ def place_order(symbol, volume, order_type, price):
         "type_filling": mt5.ACCOUNT_TRADE_MODE_DEMO,
     }
     result = mt5.order_send(request_data)
+    print(result)
+    mt5.shutdown()
     return result
 
-def close_positions(symbol_to_close):
+def close_positions(account_id, symbol_to_close):
+    mt5.login(meta_trader_accounts[account_id]['login'], meta_trader_accounts[account_id]['password'])
     positions = mt5.positions_get(symbol=symbol_to_close)
     if positions:
         for position in positions:
@@ -49,22 +86,41 @@ def close_positions(symbol_to_close):
             }
             result = mt5.order_send(close_request)
             if result.retcode != mt5.TRADE_RETCODE_DONE:
+                mt5.shutdown()
                 return "Close order failed: {}".format(result.comment)
             else:
+                mt5.shutdown()
                 return "Position {} closed successfully".format(position_id)
     else:
+        mt5.shutdown()
         return f"No positions found for symbol {symbol_to_close}"
 
 @app.route('/')
 def index():
-    balance = get_account_balance()
-    return render_template('index.html', balance=balance)
+    return render_template('index.html', meta_trader_accounts=meta_trader_accounts)
+
+@app.route('/add_account', methods=['POST'])
+def add_account():
+    login = request.form['login']
+    password = request.form['password']
+    server = request.form['server']
+    path = request.form['path']
+    account_id = len(meta_trader_accounts) + 1
+    meta_trader_accounts[account_id] = {'login': login, 'password': password, 'server': server, 'path': path}  # Store server along with other account info
+    save_meta_trader_accounts()
+    return jsonify(success=True)
+
 
 @app.route('/buy', methods=['POST'])
 def buy():
+    print("\n stat buy =>")
+    account_id = request.form['account_id']
     symbol_input = request.form['symbol']
     lot_size = float(request.form['lot_size'])
+    mt5.initialize( path=meta_trader_accounts[account_id]['path'],login=int(meta_trader_accounts[account_id]['login']), server=meta_trader_accounts[account_id]['server'],password=meta_trader_accounts[account_id]['password'])
     symbol_info = mt5.symbol_info(symbol_input)
+    print(symbol_input,symbol_info)
+    
     if symbol_info is None:
         message = "Failed to get symbol information for {}".format(symbol_input)
     elif not symbol_info.visible:
@@ -72,16 +128,17 @@ def buy():
     elif not symbol_info.bid or not symbol_info.ask:
         message = "Failed to get bid/ask prices for symbol {}".format(symbol_input)
     else:
-        result = place_order(symbol_input, lot_size, mt5.ORDER_TYPE_BUY, symbol_info.ask)
+        result = place_order(account_id, symbol_input, lot_size, mt5.ORDER_TYPE_BUY, symbol_info.ask)
         if result.retcode != mt5.TRADE_RETCODE_DONE:
             message = "Buy order failed: {}".format(result.comment)
         else:
             message = "Buy order placed successfully"
-    balance = get_account_balance()
-    return render_template('index.html', message=message, balance=balance)
+    balance = get_account_balance(account_id)
+    return jsonify(message=message, balance=balance)
 
 @app.route('/sell', methods=['POST'])
 def sell():
+    account_id = int(request.form['account_id'])
     symbol_input = request.form['symbol']
     lot_size = float(request.form['lot_size'])
     symbol_info = mt5.symbol_info(symbol_input)
@@ -92,30 +149,30 @@ def sell():
     elif not symbol_info.bid or not symbol_info.ask:
         message = "Failed to get bid/ask prices for symbol {}".format(symbol_input)
     else:
-        result = place_order(symbol_input, lot_size, mt5.ORDER_TYPE_SELL, symbol_info.bid)
+        result = place_order(account_id, symbol_input, lot_size, mt5.ORDER_TYPE_SELL, symbol_info.bid)
         if result.retcode != mt5.TRADE_RETCODE_DONE:
             message = "Sell order failed: {}".format(result.comment)
         else:
             message = "Sell order placed successfully"
-    balance = get_account_balance()
-    return render_template('index.html', message=message, balance=balance)
+    balance = get_account_balance(account_id)
+    return jsonify(message=message, balance=balance)
 
 @app.route('/close', methods=['POST'])
 def close():
+    account_id = int(request.form['account_id'])
     symbol_to_close = request.form['symbol']
-    message = close_positions(symbol_to_close)
-    balance = get_account_balance()
-    return render_template('index.html', message=message, balance=balance)
+    message = close_positions(account_id, symbol_to_close)
+    balance = get_account_balance(account_id)
+    return jsonify(message=message, balance=balance)
 
-@app.route('/metatrader-actions', methods=['POST'])
-def metatrader_actions():
-    action = request.form['action']
-    if action == 'buy':
-        return buy()
-    elif action == 'sell':
-        return sell()
-    elif action == 'close':
-        return close()
-      
+@app.route('/remove_account', methods=['POST'])
+def remove_account():
+    select_account_id = request.form['account_id']
+    for account_id in meta_trader_accounts:
+        if(account_id == select_account_id):
+            del meta_trader_accounts[account_id]
+            save_meta_trader_accounts()
+            return jsonify(success=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
